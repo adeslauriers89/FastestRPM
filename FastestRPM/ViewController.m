bd64d08030ce790f865bbd2ece2f102dc5bb6cac
adeslauriers89 <adeslauriers89@gmail.com>
2016-01-21 16:14:20 -0800

    almost done

--- FastestRPM/ViewController.m
+++ FastestRPM/ViewController.m
@@ -10,18 +10,67 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UIImageView *needleImage;
+@property (weak, nonatomic) IBOutlet UIImageView *speedometerImage;
+@property (nonatomic) CGPoint lastPoint;
+@property (nonatomic) CGPoint firstPoint;
+
+@property (strong, nonatomic) IBOutlet UIPanGestureRecognizer *moveTheNeedle;
+
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    UIImageView *needleImage = [[UIImageView alloc] init];
+  //  needleImage.center = CGPointMake(203.0, 343.0);
+    self.needleImage.transform = CGAffineTransformMakeRotation(2.65);
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (IBAction)moveTheNeedle:(UIPanGestureRecognizer*)sender {
+   
+    
+    
+    UIGestureRecognizerState state = sender.state;
+    
+    CGPoint velocity = [sender velocityInView:self.view];
+   // NSLog(@"hi %@", NSStringFromCGPoint(velocity));
+  //  NSLog(@"yo %f %f", velocity.x, velocity.y);
+    
+    float vel =  sqrtf((velocity.x * velocity.x) + (velocity.y * velocity.y));
+    NSLog(@"current vel %f", vel);
+    
+    
+    if ([sender state] == UIGestureRecognizerStateChanged) {
+        if (vel >= 3000.0) {
+            self.needleImage.transform = CGAffineTransformMakeRotation(2.2 * M_PI);
+        } else if (vel >= 2000.0){
+            self.needleImage.transform = CGAffineTransformMakeRotation(1.8 * M_PI);
+        }else if (vel >= 1000.0){
+            self.needleImage.transform = CGAffineTransformMakeRotation(1.4 * M_PI);
+        } else if (vel >= 1.0){
+            self.needleImage.transform = CGAffineTransformMakeRotation(1.0 * M_PI);
+        }
+    }
+
+   else ([sender state] == UIGestureRecognizerStateEnded) {
+        self.needleImage.transform = CGAffineTransformMakeRotation(2.65);
+        
+   }
+
+
+
+
+
+
 
 @end
